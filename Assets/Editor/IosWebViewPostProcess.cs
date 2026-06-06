a1d6b544d0d234b1a92a98eedc7837d6bef19379
EmreOzmen <emreozmen95@gmail.com>
2026-06-06 15:10:32 +0300

    Add iOS App Store icon

--- Assets/Editor/IosWebViewPostProcess.cs
+++ Assets/Editor/IosWebViewPostProcess.cs
@@ -2,6 +2,7 @@ using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
+using UnityEngine;
 
 public static class IosWebViewPostProcess
 {
@@ -29,6 +30,88 @@ public static class IosWebViewPostProcess
         project.AddFrameworkToProject(unityFrameworkGuid, "WebKit.framework", false);
         project.AddFrameworkToProject(mainTargetGuid, "WebKit.framework", false);
         project.WriteToFile(projectPath);
+
+        InjectAppStoreIcon(pathToBuiltProject);
+    }
+
+    private static void InjectAppStoreIcon(string pathToBuiltProject)
+    {
+        var sourceIcon = Path.Combine(Application.dataPath, "AppIcon", "AppIcon1024.png");
+        if (!File.Exists(sourceIcon))
+        {
+            throw new FileNotFoundException("Missing iOS App Store icon source.", sourceIcon);
+        }
+
+        var appIconSet = FindAppIconSet(pathToBuiltProject);
+        Directory.CreateDirectory(appIconSet);
+
+        const string iconFileName = "Icon-App-1024x1024@1x.png";
+        File.Copy(sourceIcon, Path.Combine(appIconSet, iconFileName), true);
+
+        var contentsPath = Path.Combine(appIconSet, "Contents.json");
+        var entry = $@"    {{
+      ""idiom"" : ""ios-marketing"",
+      ""size"" : ""1024x1024"",
+      ""scale"" : ""1x"",
+      ""filename"" : ""{iconFileName}""
+    }}";
+
+        if (!File.Exists(contentsPath))
+        {
+            File.WriteAllText(contentsPath, $@"{{
+  ""images"" : [
+{entry}
+  ],
+  ""info"" : {{
+    ""author"" : ""xcode"",
+    ""version"" : 1
+  }}
+}}
+");
+            return;
+        }
+
+        var contents = File.ReadAllText(contentsPath);
+        if (contents.Contains(@"""ios-marketing"""))
+        {
+            return;
+        }
+
+        var imageArrayEnd = contents.LastIndexOf("  ]");
+        if (imageArrayEnd < 0)
+        {
+            imageArrayEnd = contents.LastIndexOf("]");
+        }
+
+        if (imageArrayEnd < 0)
+        {
+            File.WriteAllText(contentsPath, $@"{{
+  ""images"" : [
+{entry}
+  ],
+  ""info"" : {{
+    ""author"" : ""xcode"",
+    ""version"" : 1
+  }}
+}}
+");
+            return;
+        }
+
+        var separator = contents.Contains(@"""filename""") ? ",\n" : string.Empty;
+        contents = contents.Insert(imageArrayEnd, separator + entry + "\n");
+        File.WriteAllText(contentsPath, contents);
+    }
+
+    private static string FindAppIconSet(string pathToBuiltProject)
+    {
+        var matches = Directory.GetDirectories(pathToBuiltProject, "AppIcon.appiconset", SearchOption.AllDirectories);
+        if (matches.Length > 0)
+        {
+            return matches[0];
+        }
+
+        return Path.Combine(pathToBuiltProject, "Unity-iPhone", "Images.xcassets", "AppIcon.appiconset");
     }
 
     private const string NativeSource = @"
